661b1178da0e155102cc3d5de544a0d7c5544d88
Tomoya Koyanagi <tomk79@gmail.com>
2021-06-19 12:48:58 +0900

    add: webpack

---
+++ src_front/theme.js
@@ -0,0 +1,6 @@
+const px2style = require('px2style');
+const $ = require('jquery');
+window.addEventListener('load', function(){
+    console.log('window.onload(): done.');
+	console.log(px2style);
+});
