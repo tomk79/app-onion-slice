bf8c5c895257bc0b1c41db1907bdddc19ea6b8d4
Tomoya Koyanagi <tomk79@gmail.com>
2021-06-26 18:36:06 +0900

    既存のGitリモートからセットアップできるようになった。

--- php/setup.php
+++ php/setup.php
@@ -90,11 +90,15 @@ class setup {
 			return;
 		}
 		?>
-			<p>データディレクトリを作成します。</p>
+			<p>Onion Slice のセットアップへようこそ！</p>
+			<p>はじめに、データディレクトリを作成します。</p>
+			<p>データディレクトリのパスは次の通りです。</p>
+			<pre><code><?= htmlspecialchars( $this->rencon->fs()->get_realpath($this->rencon->conf()->path_data_dir) ) ?></code></pre>
+			<p>よろしければ、「次へ」をクリックしてください。</p>
 			<form action="?" method="post">
 				<input type="hidden" name="a" value="<?= $this->rencon->req()->get_param('a') ?>" />
 				<input type="hidden" name="cmd" value="next" />
-				<button type="submit" class="px2-btn px2-btn--primary">次へ</button>
+				<p><button type="submit" class="px2-btn px2-btn--primary">次へ</button></p>
 			</form>
 		<?php
 		return;
@@ -108,20 +112,65 @@ class setup {
 			$path_composer = realpath($this->rencon->conf()->path_data_dir.'/commands/composer/composer.phar');
 			$base_dir = $this->rencon->conf()->path_data_dir.'/project/';
 			$current_dir = realpath('.');
-			chdir($base_dir);
 
-			exec($this->rencon->conf()->commands->php.' '.$path_composer.' create-project pickles2/preset-get-start-pickles2 ./');
+			if( $this->rencon->req()->get_param('setup-option') == 'pickles2' ){
+				// --------------------------------------
+				// preset-get-start-pickles2 からセットアップ
+				chdir($base_dir);
+				exec($this->rencon->conf()->commands->php.' '.$path_composer.' create-project pickles2/preset-get-start-pickles2 ./');
+				chdir($current_dir);
 
-			chdir($current_dir);
+			}elseif( $this->rencon->req()->get_param('setup-option') == 'git' ){
+				// --------------------------------------
+				// 任意のGitリモートからセットアップ
+				$gitremote = $this->rencon->req()->get_param('git-url');
+				$username = $this->rencon->req()->get_param('git-username');
+				$password = $this->rencon->req()->get_param('git-password');
+				$gitHelper = new \tomk79\onionSlice\helpers\git( $this->rencon );
+				$gitHelper->git( array(
+					'clone',
+					$gitHelper->url_bind_confidentials($gitremote, $username, $password),
+					'./',
+				) );
+				chdir($base_dir);
+				exec($this->rencon->conf()->commands->php.' '.$path_composer.' install');
+				chdir($current_dir);
+
+				$env_config = new \tomk79\onionSlice\model\env_config( $this->rencon );
+				$env_config->git_url = $this->rencon->req()->get_param('git-url');
+				$env_config->git_username = $this->rencon->req()->get_param('git-username');
+				$env_config->git_password = $this->rencon->req()->get_param('git-password');
+				$env_config->save();
+			}
+
 			$this->reload();
 			return;
 		}
 		?>
-			<p>プロジェクトをセットアップします。</p>
+			<p>続いて、プロジェクトをセットアップします。</p>
 			<form action="?" method="post">
+				<ul>
+					<li><label><input type="radio" name="setup-option" value="pickles2" checked="checked" /> Packagist から Pickles 2 プロジェクトテンプレート をセットアップ</label></li>
+					<li><label><input type="radio" name="setup-option" value="git" /> Gitリポジトリ から クローン</label>
+						<table style="width: 100%;">
+							<tr>
+								<th>Repository URL</th>
+								<td><input type="text" name="git-url" value="" class="px2-input px2-input--block" /></td>
+							</tr>
+							<tr>
+								<th>User name</th>
+								<td><input type="text" name="git-username" value="" class="px2-input px2-input--block" /></td>
+							</tr>
+							<tr>
+								<th>Password</th>
+								<td><input type="password" name="git-password" value="" class="px2-input px2-input--block" /></td>
+							</tr>
+						</table>
+					</li>
+				</ul>
 				<input type="hidden" name="a" value="<?= $this->rencon->req()->get_param('a') ?>" />
 				<input type="hidden" name="cmd" value="next" />
-				<button type="submit" class="px2-btn px2-btn--primary">次へ</button>
+				<p><button type="submit" class="px2-btn px2-btn--primary">次へ</button></p>
 			</form>
 		<?php
 		return;
@@ -150,7 +199,7 @@ class setup {
 			<form action="?" method="post">
 				<input type="hidden" name="a" value="<?= $this->rencon->req()->get_param('a') ?>" />
 				<input type="hidden" name="cmd" value="next" />
-				<button type="submit" class="px2-btn px2-btn--primary">次へ</button>
+				<p><button type="submit" class="px2-btn px2-btn--primary">次へ</button></p>
 			</form>
 		<?php
 		return;
