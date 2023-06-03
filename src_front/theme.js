9dc7eae7c5e08142e3e840f0e8eb6e5c29dfebae
Tomoya Koyanagi <tomk79@gmail.com>
2023-06-03 22:54:36 +0900

    fix: frontend

--- src_front/theme.js
+++ src_front/theme.js
@@ -1,5 +1,5 @@
 window.$ = require('jquery');
-window.px2style = new (require('px2style'))();
+require('px2style/dist/px2style.js');
 window.addEventListener('load', function(){
     console.log('window.onload(): done.', window.px2style);
 	window.px2style.header.init({'current':window.current});
