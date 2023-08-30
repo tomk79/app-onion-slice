47edb6bb007f1aa29631c7f443609b2202c94258
Tomoya Koyanagi <tomk79@gmail.com>
2023-08-30 13:51:48 +0900

    スケジュールのIDの形式を決めた

--- app/pages/scheduler/index.php
+++ app/pages/scheduler/index.php
@@ -4,4 +4,25 @@ $project_id = $rencon->get_route_param('projectId');
 $project = $projects->get($project_id);
 $scheduler = $project->scheduler();
 ?>
-<p>配信スケジュールを登録します。</p>
+
+
+<p class="px2-text-align-right"><button type="button" class="px2-btn px2-btn--primary">新規配信予定</button></p>
+<?php
+$active_schedules = $scheduler->get_schedule_all();
+if( !count($active_schedules) ){
+    echo "<p>配信予定はありません。</p>";
+}else{
+    echo '<table class="px2-table">'."\n";
+    foreach( $active_schedules as $schedule_id => $schedule_info ){
+        ?>
+        <tr>
+            <td><?= htmlspecialchars($schedule_id) ?></td>
+            <td><?= htmlspecialchars($schedule_info->release_at) ?></td>
+            <td><?= htmlspecialchars(strtotime($schedule_info->release_at)) ?></td>
+            <td><?= htmlspecialchars(date('Y-m-d H:i:s (e)', strtotime($schedule_info->release_at))) ?></td>
+        </tr>
+        <?php
+    }
+    echo '</table>'."\n";
+}
+?>
--- php/model/scheduler.php
+++ php/model/scheduler.php
@@ -20,7 +20,42 @@ class scheduler {
 		$this->realpath_project_data_dir = $this->project->get_realpath_project_data_dir();
 		if( is_string($this->realpath_project_data_dir) && is_dir($this->realpath_project_data_dir) && !is_dir($this->realpath_project_data_dir.'schedule/') ){
 			$this->rencon->fs()->mkdir($this->realpath_project_data_dir.'schedule/');
+			$this->rencon->fs()->mkdir($this->realpath_project_data_dir.'schedule/_archives/'); // 過去配信済みの予定を格納する
 		}
 		return;
+	}
+
+	/**
+	 * アクティブな配信スケジュールを全件取得する
+	 */
+	public function get_schedule_all(){
+		$rtn = array();
+		$dirs = $this->rencon->fs()->ls($this->realpath_project_data_dir.'schedule/');
+		foreach( $dirs as $dir ){
+			if( $dir == '_archives' ){
+				// アーカイブ済みの予約は除外
+				continue;
+			}
+			$schedule = (object) array();
+			$schedule->release_at = $this->parse_release_at($dir);
+			$rtn[$dir] = $schedule;
+		}
+		return $rtn;
+	}
+
+	/**
+	 * リリース予約のディレクトリ名をパースする
+	 */
+	private function parse_release_at( $dir ){
+		if( !preg_match('/^([0-9]{4})\-([0-9]{2})\-([0-9]{2})\-([0-9]{2})\-([0-9]{2})\-([0-9]{2})$/', $dir, $matched) ){
+			return false;
+		}
+		$y = $matched[1];
+		$m = $matched[2];
+		$d = $matched[3];
+		$h = $matched[4];
+		$i = $matched[5];
+		$s = $matched[6];
+		return $y.'-'.$m.'-'.$d.'T'.$h.':'.$i.':'.$s.'Z';
 	}
 }
